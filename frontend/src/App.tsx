6a68e929c571debc337d80d94db42dd80add0f73
baloogedit <baloogedit@gmail.com>
2026-06-24 11:44:17 +0300

    improve layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,17 @@ import { CarListProvider } from './contexts/CarListProvider'
 
 export function App() {
     return (
-        <FiltersProvider>
-            <CarListProvider>
-                <Content />
-            </CarListProvider>
-        </FiltersProvider>
+        <div className="AppShell">
+            <div className="AppShell__ambient AppShell__ambient--one" />
+            <div className="AppShell__ambient AppShell__ambient--two" />
+            <div className="AppShell__frame">
+                <FiltersProvider>
+                    <CarListProvider>
+                        <Content />
+                    </CarListProvider>
+                </FiltersProvider>
+            </div>
+        </div>
     )
 }
 
